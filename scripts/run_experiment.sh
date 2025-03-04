be4a6d0cdb4225f70d5b86f24a7fba8b958f48ef
Travis Whitfield <thwhitfield@gmail.com>
2025-03-03 19:49:02 -0500

    Add some additional experiments

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -5,9 +5,16 @@
 
 # python trav_nlp/pipeline.py
 # python trav_nlp/pipeline.py experiment.submit_to_kaggle=true
-python trav_nlp/pipeline.py embeddings=glove1 embeddings.gensim_embedding_name=glove-twitter-25
-python trav_nlp/pipeline.py embeddings=glove1 embeddings.gensim_embedding_name=glove-twitter-50
-python trav_nlp/pipeline.py embeddings=glove1 embeddings.gensim_embedding_name=glove-twitter-100
-python trav_nlp/pipeline.py embeddings=glove1 embeddings.gensim_embedding_name=glove-twitter-200
+# python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-twitter-25
+# python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-twitter-50
+# python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-twitter-100
+# python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-twitter-200
 
-
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=fasttext-wiki-news-subwords-300
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=conceptnet-numberbatch-17-06-300
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=word2vec-ruscorpora-300
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=word2vec-google-news-300
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-wiki-gigaword-50
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-wiki-gigaword-100
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-wiki-gigaword-200
+python trav_nlp/pipeline.py embeddings=glove1 embeddings.name=glove-wiki-gigaword-300
